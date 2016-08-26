6602ae8be19adc608f9e68d77fa7eb059f06e4a0
Niyas C <niyasmonc@gmail.com>
2016-08-26 10:15:40 +0000

    Add page loading animation
    - Start with responsive menu

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,4 @@
-var app = angular.module("Porfolio", ['ngRoute'])
+var app = angular.module("Porfolio", ['ngRoute', 'angular-loading-bar'])
 
 app.config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/', {
